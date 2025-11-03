3a3e3fc0ffdc7605706a1df94d056b33b440c958
Blas <slab10000@gmail.com>
2025-11-03 03:26:52 -0600

    Export modules to reuse in other files

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+import {log} from "./logger.js";
+
 function sayHello(name){
     console.log("hello " + name );
 }
 
 sayHello("Blas");
+
+log("Hi this is a message")
---
+++ logger.js
@@ -0,0 +1,8 @@
+var url = 'http://mylogger.io/log';
+
+function log(message){
+    //send HTTP request
+    console.log(message);
+}
+
+export {log};
